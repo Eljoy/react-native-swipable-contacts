0617fddec9e879104b646a74708152604d96e866
ilia gazdaliev <ilia.gazdaliev@maxiru.com>
2021-11-26 11:33:52 +0500

    Reworked api on using json data

--- app/api/ProfileApi.ts
+++ app/api/ProfileApi.ts
@@ -1,16 +1,6 @@
+import profilesJson from '../../profile-data.json'
 import { Profile } from '../models'
-import * as faker from 'faker'
 
 export function fetchProfiles(): Profile[] {
-  const profiles: Profile[] = []
-  for (let i = 0; i < 20; i++) {
-    profiles.push({
-      id: faker.datatype.uuid(),
-      firstName: faker.name.firstName(),
-      lastName: faker.name.lastName(),
-      imageUrl: faker.image.avatar(),
-      about: faker.lorem.paragraph(),
-    })
-  }
-  return profiles
+  return profilesJson.map((pJson) => Profile.deserialize(pJson))
 }

d0d48bdb1067090b65ad5957f29488951093c9fd
ilia gazdaliev <ilia.gazdaliev@maxiru.com>
2021-11-30 22:12:06 +0500

    Added BottomShadow show on ProfileInfoScrollView drag

--- app/screens/ProfileScreen.tsx
+++ app/screens/ProfileScreen.tsx
@@ -1,9 +1,16 @@
 import React, { useEffect } from 'react'
-import { runOnJS, useSharedValue } from 'react-native-reanimated'
+import { StyleSheet, ViewStyle } from 'react-native'
+import {
+  runOnJS,
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from 'react-native-reanimated'
 import { Header } from '../components/header'
 import { Layout } from '../components/layout'
 import { ProfileAvatarsScrollView } from '../components/profile'
 import { ProfileInfoScrollView } from '../components/profile/ProfileInfoScrollView'
+import { BottomShadow } from '../components/shadow'
 import { TitleText } from '../components/typography'
 import { useProfile } from '../hooks/useProfile'
 import { Profile } from '../models'
@@ -20,6 +27,7 @@ export default function ProfileScreen() {
     'worklet'
     profileAvatarsScrollIndex.value = index
     profileInfoScrollIndex.value = index
+    runOnJS(selectProfile)(profiles[index])
   }
 
   function onAvatarsIndexChanged(index: number) {
@@ -30,24 +38,63 @@ export default function ProfileScreen() {
 
   function onProfileSelect(profile: Profile, index: number) {
     'worklet'
-    onProfileInfoScrollIndexChanged(index)
+    profileAvatarsScrollIndex.value = index
+    profileInfoScrollIndex.value = index
     selectProfile(profile)
   }
 
+  const isProfileInfoDragged = useSharedValue(false)
+
+  function onProfileScrollDragBegin() {
+    'worklet'
+    isProfileInfoDragged.value = true
+  }
+
+  function onProfileScrollDragEnd() {
+    'worklet'
+    isProfileInfoDragged.value = false
+  }
+
+  const shadowAnimatedStyle = useAnimatedStyle<ViewStyle>(() => {
+    return {
+      shadowOpacity: isProfileInfoDragged.value
+        ? withSpring(styles.shadow.shadowOpacity)
+        : withSpring(0),
+    }
+  })
+
   return (
-    <Layout flex={1}>
+    <Layout flex={1} style={{ backgroundColor: 'white' }}>
       <Header>
         <TitleText style={{ fontWeight: 'bold' }}>Contacts</TitleText>
       </Header>
-      <ProfileAvatarsScrollView
-        scrollIndex={profileAvatarsScrollIndex}
-        onIndexChanged={onAvatarsIndexChanged}
-        onProfileSelect={onProfileSelect}
-      />
+      <BottomShadow
+        containerStyle={{ height: 125 }}
+        shadowWidth={5}
+        shadowStyle={[styles.shadow, shadowAnimatedStyle]}>
+        <ProfileAvatarsScrollView
+          scrollIndex={profileAvatarsScrollIndex}
+          onIndexChanged={onAvatarsIndexChanged}
+          onProfileSelect={onProfileSelect}
+        />
+      </BottomShadow>
       <ProfileInfoScrollView
+        onBeginDrag={onProfileScrollDragBegin}
+        onEndDrag={onProfileScrollDragEnd}
         scrollIndex={profileInfoScrollIndex}
         onIndexChanged={onProfileInfoScrollIndexChanged}
       />
     </Layout>
   )
 }
+
+const styles = StyleSheet.create({
+  shadow: {
+    shadowOffset: {
+      width: 0,
+      height: -2,
+    },
+    shadowOpacity: 0.27,
+    shadowRadius: 4.65,
+  },
+})
